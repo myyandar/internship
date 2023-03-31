b8f1ffe8a0546e811ef25474b0ee6264dee5aa84
myyandar <myyandar@hotmail.com>
2023-03-30 23:51:47 -0300

    change logo and link

---
+++ changelogo.sh
@@ -0,0 +1,8 @@
+git checkout -b update_logo
+sed -i "" 's|img src={logo}|img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"|g' src/App.js
+sed -i "" 's|href="https://reactjs.org"|href="https://www.propelleraero.com/dirtmate/"|g' src/App.
+git add .
+git commit -m "change logo and link"
+git push origin update_logo
+gh pr create --title "Update Logo" --body "Updating logo and link"
+gh pr merge
